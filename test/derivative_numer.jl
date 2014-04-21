8f8ab91656c77b957f8b18c087a7f09cafd0ebe9
Tomas Lycken <tomas.lycken@gmail.com>
2014-04-21 03:51:28 +0200

    Added second order numerical derivatives for testing

--- test/derivative_numer.jl
+++ test/derivative_numer.jl
@@ -7,7 +7,11 @@ function derivative_numer{T<:Number}(func::Function, x::T, h::T)
     vm = func(xm)
     return (vp-vm)/(xp-xm)
 end
+function derivative2_numer{T<:Number}(func::Function, x::T, h::T)
+    return (func(x + 2h) - 2*func(x) + func(x - 2h)) / 4h^2
+end
 derivative_numer{T<:Number}(func::Function, x::T, index::Int, h::T) = derivative_numer(func, x, h)  # compatibility with partial derivatives
+derivative2_numer{T<:Number}(func::Function, x::T, index::Int, h::T) = derivative2_numer(func, x, h)
 function derivative_numer{T<:Number}(func::Function, x::Array{T}, index::Int, h::T)
     xsave = x[index]
     xp = xsave + h
@@ -18,6 +22,27 @@ function derivative_numer{T<:Number}(func::Function, x::Array{T}, index::Int, h:
     vm = func(x)
     x[index] = xsave
     return (vp-vm)/(xp-xm)
+end
+function derivative2_numer{T<:Number}(func::Function, x::Array{T}, index::(Int,Int,), h::T)
+    if index[1] == index[2] # 2nd order derivative in one direction
+        v0 = func(x...)
+        xsave = x[index[1]]
+        xp = xsave + 2h
+        xm = xsave - 2h
+        x[index[1]] = xp
+        vp = func(x...)
+        x[index[1]] = xm
+        vm = func(x...)
+        x[index[1]] = xsave
+        deriv = (vp - 2v0 + vm) / 4h^2
+    else # cross-derivative, 1st order in each direction
+        xpp = [x[1]+h,x[2]+h]
+        xpm = [x[1]+h,x[2]-h]
+        xmp = [x[1]-h,x[2]+h]
+        xmm = [x[1]-h,x[2]-h]
+        deriv = (func(xpp...) - func(xpm...) - func(xmp...) + func(xmm...)) / 4h^2
+    end
+    return deriv
 end
 function derivative_numer{T<:Number}(func::Function, c::(T...,), index::Int, h::T)
     x = [c...]
@@ -31,10 +56,21 @@ function derivative_numer{T<:Number}(func::Function, c::(T...,), index::Int, h::
     x[index] = xsave
     return (vp-vm)/(xp-xm)
 end
+function derivative2_numer{T<:Number}(func::Function, c::(T...,), index::(Int,Int,), h::T)
+    x = [c...]
+    return derivative2_numer(func, x, index, h)
+end
 function derivative_numer{T<:Number}(func::Function, x::T, h::Vector{T})
     d = zeros(T, length(h))
     for i = 1:length(h)
         d[i] = derivative_numer(func, x, h[i])
+    end
+    return d
+end
+function derivative2_numer{T<:Number}(func::Function, x, h::Vector{T})
+    d = zeros(T, length(h))
+    for i = 1:length(h)
+        d[i] = derivative2_numer(func, x, h[i])
     end
     return d
 end
@@ -45,6 +81,16 @@ function derivative_numer{T<:Number}(func::Function, x, index::Int, h::Vector{T}
     end
     return d
 end
+function derivative2_numer{T<:Number}(func::Function, x, index::(Int,Int,), h::Vector{T})
+    d = zeros(T, length(h))
+    for i = 1:length(h)
+        d[i] = derivative2_numer(func, x, index, h[i])
+    end
+    return d
+end
 derivative_numer{T<:Number}(func::Function, x::T) = derivative_numer(func, x, (eps(max(abs(x),one(T))))^convert(T, 1/3))
 derivative_numer{T<:Number}(func::Function, x::Array{T}, index::Int) = derivative_numer(func, x, index, (eps(max(abs(x[index]),one(T))))^convert(T, 1/3))
 derivative_numer{T<:Number}(func::Function, c::(T...,), index::Int) = derivative_numer(func, c, index, (eps(max(abs(c[index]),one(T))))^convert(T, 1/3))
+derivative2_numer{T<:Number}(func::Function, x::T) = derivative2_numer(func, x, (eps(max(abs(x),one(T))))^convert(T,1/3))
+derivative2_numer{T<:Number}(func::Function, x::Array{T}, index::(Int,Int,)) = derivative2_numer(func, x, index, (eps(max(abs(x[index[1]]),one(T))))^convert(T, 1/3))
+derivative2_numer{T<:Number}(func::Function, c::(T...,), index::(Int,Int,)) = derivative2_numer(func, c, index, (eps(max(abs(c[index[1]]),one(T))))^convert(T, 1/3))
